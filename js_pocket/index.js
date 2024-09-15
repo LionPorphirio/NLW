ebb77bd0602ca30d55b8ba41e89983530fab958b
Lion Porphirio <lion.porphirio@gmail.com>
2024-09-15 18:48:12 -0300

    sistema de carregar e salvar metas

--- js_pocket/index.js
+++ js_pocket/index.js
@@ -1,12 +1,23 @@
 const { select, input, checkbox } = require('@inquirer/prompts')
+const fs = require('fs').promises
 
 let mensagem = 'Bem-vindo ao app de metas!'
-let meta = {
-    value: 'Beber 3l de água por dia',
-    checked: false
+
+let metas
+
+const carregarMetas = async () => {
+    try{
+        const dados = await fs.readFile('metas.json', 'utf-8')
+        metas = JSON.parse(dados)
+    }
+    catch(erro) {
+        metas = []
+    }
 }
 
-let metas = [ meta ]
+const salvarMetas = async () => {
+    await fs.writeFile('metas.json', JSON.stringify(metas, null, 2))
+}
 
 const cadastrarMeta = async () => {
     const meta = await input({message: 'Digite a meta:'})
@@ -116,9 +127,12 @@ const mostrarMensagem = () => {
 
 const start = async () => {
 
+    await carregarMetas()
+
     while(true) {
 
         mostrarMensagem()
+        await salvarMetas()
 
         const opcao = await select({
             message: 'menu >',
